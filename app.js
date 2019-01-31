c671de68aa0b9bb2636fa3339873a76b28d6a87c
gdegans <47058033+gdegans@users.noreply.github.com>
2019-01-31 03:45:37 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+/* AMÉLIORATIONS :
+
+  - Système de level up :
+    - Pour le joueur
+    - Niveau des monstres et caractéristiques scalés
+    - Aggrandissement de la map ?
+
+  - Système d'inventaire :
+    - Inventaire pour le joueur
+    - Inventaire pour les ennemis
+    - Qui peuvent posséder potions, or, équipement
+
+  - Système d'équipement
+
+*/
+
 /* Quand le client se connecte, joueur est créé côté serveur et est envoyé
 Lorqu'il demande_combat, il envoie joueur au serveur et ennemi est créé, tout est renvoyé
 */
@@ -13,26 +29,6 @@ Lorqu'il demande_combat, il envoie joueur au serveur et ennemi est créé, tout 
 var positionX = 1;
 var positionY = 1;
 var map = [];
-//map[1] = [];
-//map[1][1] = "Case vide";
-/* map[1][1] = ;
-map[1][2] = ;
-map[1][3] = ;
-map[1][4] = ;
-map[2][1] = ;
-map[2][2] = ;
-map[2][3] = ;
-map[2][4] = ;
-map[3][1] = ;
-map[3][2] = ;
-map[3][3] = ;
-map[3][4] = ;
-map[3][1] = ;
-map[3][2] = ;
-map[3][3] = ;
-map[3][4] = ; */
-
-// console.log(CreerMap(3, 3));
 
 var app = require('express')();
 var server = require('http').createServer(app);
@@ -77,10 +73,14 @@ class Ennemi {
   }
 }
 
-function AjouterEnnemi(xmax, ymax) {
+function AjouterEnnemiSurMap(xmax, ymax) {
   positionXEnnemi = Aleatoire(1, xmax);
   positionYEnnemi = Aleatoire(1, ymax);
-  map[positionXEnnemi][positionYEnnemi] = "X";
+  while (map[positionXEnnemi][positionYEnnemi] != "") {
+    positionXEnnemi = Aleatoire(1, xmax);
+    positionYEnnemi = Aleatoire(1, ymax);
+  }
+  map[positionXEnnemi][positionYEnnemi] = "Rat mutant";
 }
 
 function Aleatoire(min, max) {
@@ -100,7 +100,7 @@ function Attaquer(attaquant, cible) {
 }
 
 function CheckDeplacementJoueur(map, joueur) {
-  if (map[joueur.positionX][joueur.positionY] === "X") {
+  if (map[joueur.positionX][joueur.positionY] === "Rat mutant") {
     map[joueur.positionX][joueur.positionY] = "Baston";
     check = 1;
     CreerEnnemi();
@@ -113,7 +113,7 @@ function CheckDeplacementJoueur(map, joueur) {
 }
 
 function CreerJoueur(pseudo) {
-  joueur = new Joueur("", 1, 10, 5, 3);
+  joueur = new Joueur("", 1, 10, 5, 2);
   joueur.nom = pseudo;
   joueur.pv += Aleatoire(1, 3);
   joueur.force += Aleatoire(1, 3);
@@ -121,7 +121,7 @@ function CreerJoueur(pseudo) {
 }
 
 function CreerEnnemi() {
-  ennemi = new Ennemi("Rat mutant", 1, 8, 2, 1);
+  ennemi = new Ennemi("Rat mutant", 1, 8, 3, 1);
   ennemi.pv += Aleatoire(1, 3);
   ennemi.force += Aleatoire(1, 3);
   ennemi.defense += Aleatoire(1, 3);
@@ -165,83 +165,83 @@ io.sockets.on('connection', function(socket) { // Connexion
   socket.on('creation_joueur', function(pseudo) { // Lorsqu'un client arrive...
     CreerJoueur(pseudo);
     socket.emit('envoi_joueur', { joueurServeur: joueur}); // On lui envoie la liste
-//    console.log("Création du personnage : OK");
+    //console.log("Création du personnage : OK");
   });
 
-  socket.on('demande_map', function(map) {
+  socket.on('creation_map', function(map) {
     x = 9;
     y = 9;
     map = CreerMap(x, y);
-    AjouterEnnemi(x, y);
-    AjouterEnnemi(x, y);
-    AjouterEnnemi(x, y);
-    AjouterEnnemi(x, y);
     LocaliserJoueur(joueur, map);
+    i = 0;
+    while (i < 10) {
+      AjouterEnnemiSurMap(x, y);
+      i++
+    }
     socket.emit('envoi_map', { mapServeur: map});
     console.log(joueur.positionY);
   });
 
+  socket.on('actualiser_map', function() {
+    socket.emit('envoi_map', { mapServeur: map});
+  });
+
   socket.on('deplacement_joueur', function(nomTouche) {
     console.log(map);
-    if (joueur.enCombat === 0) {
-
+    if (joueur.enCombat === 0) { // Si le joueur n'est pas en combat, on prend on compte les input
       switch(nomTouche) {
-
         case "ArrowUp":
-        console.log("Le joueur doit aller en haut si possible");
-        if (joueur.positionY > 1) {
-        //map[joueur.positionX][joueur.positionY] = joueur.positionX + ', ' + joueur.positionY;
-        map[joueur.positionX][joueur.positionY] = "";
-        joueur.positionY--;
-        CheckDeplacementJoueur(map, joueur);
-        if (check === 1) {
-        socket.emit('nouvel_ennemi', { joueurServeur: joueur, ennemiServeur: ennemi}); // On lui envoie la liste
-        }
-        socket.emit('envoi_map', { mapServeur: map});
-        }
-        break;
-
+          console.log("Le joueur doit aller en haut si possible");
+          if (joueur.positionY > 1) { // Si le joueur n'est pas sur la première ligne
+            //map[joueur.positionX][joueur.positionY] = joueur.positionX + ', ' + joueur.positionY;
+            map[joueur.positionX][joueur.positionY] = "";
+            joueur.positionY--;
+            CheckDeplacementJoueur(map, joueur);
+            if (check === 1) {
+              socket.emit('nouvel_ennemi', { joueurServeur: joueur, ennemiServeur: ennemi}); // On lui envoie la liste
+            }
+            socket.emit('envoi_map', { mapServeur: map});
+          }
+          break;
         case "ArrowRight":
-        console.log("Le joueur doit aller à droite si possible");
-        if (joueur.positionX < map.xmax) {
-        //map[joueur.positionX][joueur.positionY] = joueur.positionX + ', ' + joueur.positionY;
-        map[joueur.positionX][joueur.positionY] = "";
-        joueur.positionX++;
-        CheckDeplacementJoueur(map, joueur);
-        if (check === 1) {
-        socket.emit('nouvel_ennemi', { joueurServeur: joueur, ennemiServeur: ennemi}); // On lui envoie la liste
-        }
-        socket.emit('envoi_map', { mapServeur: map});
-        }
-        break;
-
+          console.log("Le joueur doit aller à droite si possible"); // Si le joueur n'est pas sur la dernière colonne
+          if (joueur.positionX < map.xmax) {
+            //map[joueur.positionX][joueur.positionY] = joueur.positionX + ', ' + joueur.positionY;
+            map[joueur.positionX][joueur.positionY] = "";
+            joueur.positionX++;
+            CheckDeplacementJoueur(map, joueur);
+            if (check === 1) {
+              socket.emit('nouvel_ennemi', { joueurServeur: joueur, ennemiServeur: ennemi}); // On lui envoie la liste
+            }
+            socket.emit('envoi_map', { mapServeur: map});
+          }
+          break;
         case "ArrowDown":
-        console.log("Le joueur doit aller en bas si possible");
-        if (joueur.positionY < map.ymax) {
-        //map[joueur.positionX][joueur.positionY] = joueur.positionX + ', ' + joueur.positionY;
-        map[joueur.positionX][joueur.positionY] = "";
-        joueur.positionY++;
-        CheckDeplacementJoueur(map, joueur);
-        if (check === 1) {
-        socket.emit('nouvel_ennemi', { joueurServeur: joueur, ennemiServeur: ennemi}); // On lui envoie la liste
-        }
-        socket.emit('envoi_map', { mapServeur: map});
-        }
-        break;
-
+          console.log("Le joueur doit aller en bas si possible"); // Si le joueur n'est pas sur la dernière ligne
+          if (joueur.positionY < map.ymax) {
+            //map[joueur.positionX][joueur.positionY] = joueur.positionX + ', ' + joueur.positionY;
+            map[joueur.positionX][joueur.positionY] = "";
+            joueur.positionY++;
+            CheckDeplacementJoueur(map, joueur);
+            if (check === 1) {
+              socket.emit('nouvel_ennemi', { joueurServeur: joueur, ennemiServeur: ennemi}); // On lui envoie la liste
+            }
+            socket.emit('envoi_map', { mapServeur: map});
+          }
+          break;
         case "ArrowLeft":
-        console.log("Le joueur doit aller à gauche si possible");
-        if (joueur.positionX > 1) {
-        //map[joueur.positionX][joueur.positionY] = joueur.positionX + ', ' + joueur.positionY;
-        map[joueur.positionX][joueur.positionY] = "";
-        joueur.positionX--;
-        CheckDeplacementJoueur(map, joueur);
-        if (check === 1) {
-        socket.emit('nouvel_ennemi', { joueurServeur: joueur, ennemiServeur: ennemi}); // On lui envoie la liste
-        }
-        socket.emit('envoi_map', { mapServeur: map});
-        }
-        break;
+          console.log("Le joueur doit aller à gauche si possible"); // Si le joueur n'est pas sur la première colonne
+          if (joueur.positionX > 1) {
+            //map[joueur.positionX][joueur.positionY] = joueur.positionX + ', ' + joueur.positionY;
+            map[joueur.positionX][joueur.positionY] = "";
+            joueur.positionX--;
+            CheckDeplacementJoueur(map, joueur);
+            if (check === 1) {
+              socket.emit('nouvel_ennemi', { joueurServeur: joueur, ennemiServeur: ennemi}); // On lui envoie la liste
+            }
+            socket.emit('envoi_map', { mapServeur: map});
+          }
+          break;
       }
       console.log(nomTouche);
       console.log(map);
@@ -252,11 +252,11 @@ io.sockets.on('connection', function(socket) { // Connexion
     joueur = data.joueurClient;
     joueur.nbAttaque = 0;
     joueur.enCombat = 1;
-//    console.log('Demande de combat reçue');
+    //console.log('Demande de combat reçue');
     CreerEnnemi();
     socket.emit('nouvel_ennemi', { joueurServeur: joueur, ennemiServeur: ennemi}); // On lui envoie la liste
     socket.emit('actualiser', { joueurServeur: joueur, ennemiServeur: ennemi});
-//    console.log(joueur.pv);
+    //console.log(joueur.pv);
   });
 
   socket.on('actualiser', function(data) {
@@ -267,25 +267,30 @@ io.sockets.on('connection', function(socket) { // Connexion
   socket.on('attaque', function(data) {
     joueur = data.joueurClient;
     ennemi = data.ennemiClient;
-//    console.log(ennemi);
+    //console.log(ennemi);
     Attaquer(joueur, ennemi);
     Attaquer(ennemi, joueur);
     console.log(joueur.nbAttaque);
-//    console.log(ennemi);
-//    console.log("fin attaque");
+    //console.log(ennemi);
+    //console.log("fin attaque");
     socket.emit('actualiser', { joueurServeur: joueur, ennemiServeur: ennemi});
     if(joueur.pv === 0) {
+      map[joueur.positionX][joueur.positionY] = "Mort";
+      socket.emit('envoi_map', { mapServeur: map});
       socket.emit('mort_joueur', joueur);
     }
     else if(ennemi.pv === 0) {
       joueur.xp += ennemi.xp;
-  //    joueur.enCombat = 0;
-  //    ennemi.xp = 0;
+      //joueur.enCombat = 0;
+      //ennemi.xp = 0;
+      socket.emit('actualiser', { joueurServeur: joueur, ennemiServeur: ennemi});
       socket.emit('mort_ennemi', { joueurServeur: joueur, ennemiServeur: ennemi});
-      AjouterEnnemi(x, y);
+      AjouterEnnemiSurMap(x, y);
+      map[joueur.positionX][joueur.positionY] = "Joueur";
       socket.emit('actualiser', { joueurServeur: joueur, ennemiServeur: ennemi});
 
-  //    ennemi.force = 0;
+      //socket.emit('actualiser_map');
+      //ennemi.force = 0;
     }
   });
 
